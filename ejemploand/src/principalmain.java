31adc85dd4ea287fb080ef9cc0df27074c016e5a
Juan Garzon <jpgarzon@javeriana.edu.co>
2013-09-24 22:37:57 -0500

    segundo cambio

--- ejemploand/src/principalmain.java
+++ ejemploand/src/principalmain.java
@@ -8,6 +8,7 @@ public class principalmain {
 		// TODO Auto-generated method stub
 		System.out.println("ejemplo");
 		//un comentario para subir al repos
+		System.out.println("otro mensaje");
 	}
 
 }
