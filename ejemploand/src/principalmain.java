b9149eea8a6e8b83b59e1c21bc4b71e4d4e4640a
Juan Garzon <jpgarzon@javeriana.edu.co>
2013-09-24 22:36:44 -0500

    primer cambio

--- ejemploand/src/principalmain.java
+++ ejemploand/src/principalmain.java
@@ -7,6 +7,7 @@ public class principalmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ejemplo");
+		//un comentario para subir al repos
 	}
 
 }
